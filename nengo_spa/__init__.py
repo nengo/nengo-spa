nengo/nengo

Allow hierarchical SPA modules.
Fixes #556, #749.

nengo/spa/__init__.py
@@ -7,9 +7,10 @@ from .compare import Compare
 from .cortical import Cortical
 from .input import Input
 from .memory import Memory
+from .module import Module
 from .pointer import SemanticPointer
 from .spa import SPA
 from .state import State
 from .thalamus import Thalamus
-from .utils import enable_spa_params, similarity
-from .vocab import Vocabulary
+from .utils import similarity
+from .vocab import Vocabulary, VocabularySet

nengo/spa/spa.py
@@ -1,16 +1,10 @@
-import warnings
-
 import numpy as np
 
-import nengo
-from nengo.exceptions import SpaModuleError
-from nengo.spa.vocab import Vocabulary
+from nengo.spa.vocab import VocabularySet
 from nengo.spa.module import Module
-from nengo.spa.utils import enable_spa_params
-from nengo.utils.compat import iteritems
 
 
-class SPA(nengo.Network):
+class SPA(Module):
     """Base class for SPA models.
 
     This expands the standard Network system to support structured connections
@@ -81,147 +75,16 @@ class SPA(nengo.Network):
 
     def __init__(self, label=None, seed=None, add_to_container=None,
                  vocabs=[]):
-        super(SPA, self).__init__(label, seed, add_to_container)
-        enable_spa_params(self)
-        self._modules = {}
-        self._default_vocabs = {}
-
-        for vo in vocabs:
-            if vo.dimensions in self._default_vocabs:
-                warnings.warn("Duplicate vocabularies with dimension %d. "
-                              "Using the last entry in the vocab list with "
-                              "that dimensionality." % (vo.dimensions))
-            self._default_vocabs[vo.dimensions] = vo
-
-    def __setattr__(self, key, value):
-        """A setattr that handles Modules being added specially.
-
-        This is so that we can use the variable name for the Module as
-        the name that all of the SPA system will use to access that module.
-        """
-        if hasattr(self, key) and isinstance(getattr(self, key), Module):
-            raise SpaModuleError("Cannot re-assign module-attribute %s to %s. "
-                                 "SPA module-attributes can only be assigned "
-                                 "once." % (key, value))
-        super(SPA, self).__setattr__(key, value)
-        if isinstance(value, Module):
-            if value.label is None:
-                value.label = key
-            self._modules[key] = value
-            for k, (obj, v) in iteritems(value.inputs):
-                if type(v) == int:
-                    value.inputs[k] = (obj, self.get_default_vocab(v))
-                self.config[obj].vocab = value.inputs[k][1]
-            for k, (obj, v) in iteritems(value.outputs):
-                if type(v) == int:
-                    value.outputs[k] = (obj, self.get_default_vocab(v))
-                self.config[obj].vocab = value.outputs[k][1]
-
-            value.on_add(self)
-
-    def __exit__(self, ex_type, ex_value, traceback):
-        super(SPA, self).__exit__(ex_type, ex_value, traceback)
-        if ex_type is not None:
-            # re-raise the exception that triggered this __exit__
-            return False
-
-        module_list = frozenset(self._modules.values())
-        for net in self.networks:
-            # Since there are no attributes to distinguish what's been added
-            # and what hasn't, we have to ask the network
-            if isinstance(net, Module) and (net not in module_list):
-                raise SpaModuleError("%s must be set as an attribute of "
-                                     "a SPA network" % (net))
-
-    def get_module(self, name):
-        """Return the module for the given name."""
-        if name in self._modules:
-            return self._modules[name]
-        elif '_' in name:
-            module, name = name.rsplit('_', 1)
-            if module in self._modules:
-                return self._modules[module]
-        raise SpaModuleError("Could not find module %r" % name)
+        if seed is not None:
+            rng = np.random.RandomState(seed)
+        else:
+            rng = None
+        super(SPA, self).__init__(
+            label, seed, add_to_container, VocabularySet(vocabs, rng))
 
     def get_default_vocab(self, dimensions):
         """Return a Vocabulary with the desired dimensions.
 
         This will create a new default Vocabulary if one doesn't exist.
         """
-
-        # If seed is set, create rng based off that seed.
-        # Otherwise, just use the default NumPy rng.
-        rng = None if self.seed is None else np.random.RandomState(self.seed)
-
-        if dimensions not in self._default_vocabs:
-            self._default_vocabs[dimensions] = Vocabulary(dimensions, rng=rng)
-        return self._default_vocabs[dimensions]
-
-    def get_module_input(self, name):
-        """Return the object to connect into for the given name.
-
-        The name will be either the same as a module, or of the form
-        <module_name>_<input_name>.
-        """
-        if name in self._modules and 'default' in self._modules[name].inputs:
-            return self._modules[name].inputs['default']
-        elif '_' in name:
-            module, name = name.rsplit('_', 1)
-            if module in self._modules:
-                m = self._modules[module]
-                if name in m.inputs:
-                    return m.inputs[name]
-        raise SpaModuleError("Could not find module input %r" % name)
-
-    def get_module_inputs(self):
-        for name, module in iteritems(self._modules):
-            for input in module.inputs:
-                if input == 'default':
-                    yield name
-                else:
-                    yield '%s_%s' % (name, input)
-
-    def get_input_vocab(self, name):
-        return self.get_module_input(name)[1]
-
-    def get_module_output(self, name):
-        """Return the object to connect into for the given name.
-
-        The name will be either the same as a module, or of the form
-        <module_name>_<output_name>.
-        """
-        if name in self._modules:
-            return self._modules[name].outputs['default']
-        elif '_' in name:
-            module, name = name.rsplit('_', 1)
-            if module in self._modules:
-                m = self._modules[module]
-                if name in m.outputs:
-                    return m.outputs[name]
-        raise SpaModuleError("Could not find module output %r" % name)
-
-    def get_module_outputs(self):
-        for name, module in iteritems(self._modules):
-            for output in module.outputs:
-                if output == 'default':
-                    yield name
-                else:
-                    yield '%s_%s' % (name, output)
-
-    def get_output_vocab(self, name):
-        return self.get_module_output(name)[1]
-
-    def similarity(self, data, probe, vocab=None):
-        """Return the similarity between the probed data and corresponding
-        vocabulary.
-
-        Parameters
-        ----------
-        data: ProbeDict
-            Collection of simulation data returned by sim.run() function call.
-        probe: Probe
-            Probe with desired data.
-        """
-        if vocab is None:
-            vocab = self.config[probe.target].vocab
-        return nengo.spa.similarity(data[probe], vocab)
+        return self.vocabs.get_or_create(dimensions)

nengo/spa/tests/test_vocabulary.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 from nengo.exceptions import SpaParseError, ValidationError
-from nengo.spa import Vocabulary
+from nengo.spa import Vocabulary, VocabularySet
 from nengo.utils.testing import warns
 
 
@@ -246,3 +246,47 @@ def test_extend(rng):
     v.extend(['G', 'H'], unitary=True)
     assert v.keys == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     assert v.unitary == ['E', 'G', 'H']
+
+
+def test_vocabulary_set(rng):
+    v8 = Vocabulary(8)
+    v16 = Vocabulary(16)
+    v32 = Vocabulary(32)
+    vs = VocabularySet([v8, v16], rng=rng)
+
+    # Behaviour common to set and dict
+    assert len(vs) == 2
+    assert 8 in vs
+    assert 16 in vs
+    assert 32 not in vs
+
+    assert v8 in vs
+    assert v16 in vs
+    assert v32 not in vs
+    assert Vocabulary(8) not in vs
+
+    # dict behaviour
+    assert vs[8] is v8
+    assert vs[16] is v16
+
+    del vs[8]
+    assert 8 not in vs
+
+    # set behaviour
+    vs.add(v32)
+    assert vs[32] is v32
+    with pytest.warns(UserWarning):
+        vs.add(v32)
+
+    vs.discard(32)
+    assert 32 not in vs
+    vs.discard(v16)
+    assert 16 not in vs
+
+    # creating new vocabs if non existent
+    vs.add(v8)
+    assert vs.get_or_create(8) is v8
+    new = vs.get_or_create(16)
+    assert vs[16] is new
+    assert new.dimensions == 16
+    assert new.rng is rng

nengo/spa/vocab.py
@@ -1,3 +1,4 @@
+from collections import Mapping
 import warnings
 
 import numpy as np
@@ -483,15 +484,63 @@ class Vocabulary(object):
         return subset
 
 
-class VocabularyParam(nengo.params.Parameter):
-    """Can be a Vocabulary."""
+class VocabularySet(Mapping):
+    def __init__(self, vocabs=None, rng=None):
+        if vocabs is None:
+            vocabs = []
+        self.rng = rng
 
-    def validate(self, instance, vocab):
-        super(VocabularyParam, self).validate(instance, vocab)
+        self._vocabs = {}
+        for vo in vocabs:
+            self.add(vo)
 
-        if vocab is not None and not isinstance(vocab, Vocabulary):
-            raise ValidationError("Must be of type 'Vocabulary' (got type %r)."
-                                  % vocab.__class__.__name__,
-                                  attr=self.name, obj=instance)
+    def add(self, vocab):
+        if vocab.dimensions in self._vocabs:
+            warnings.warn("Duplicate vocabularies with dimension %d. "
+                          "Using the last entry in the vocab list with "
+                          "that dimensionality." % (vocab.dimensions))
+        self._vocabs[vocab.dimensions] = vocab
 
-        return vocab
+    def __delitem__(self, dimensions):
+        del self._vocabs[dimensions]
+
+    def discard(self, vocab):
+        if isinstance(vocab, int):
+            del self._vocabs[vocab]
+        elif self._vocabs.get(vocab.dimensions, None) is vocab:
+            del self._vocabs[vocab.dimensions]
+
+    def __getitem__(self, dimensions):
+        return self._vocabs[dimensions]
+
+    def get_or_create(self, dimensions):
+        if dimensions not in self._vocabs:
+            self._vocabs[dimensions] = Vocabulary(dimensions, rng=self.rng)
+        return self._vocabs[dimensions]
+
+    def __iter__(self):
+        return iter(self._vocabs)
+
+    def __len__(self):
+        return len(self._vocabs)
+
+    def __contains__(self, vocab):
+        if isinstance(vocab, int):
+            return vocab in self._vocabs
+        else:
+            return (vocab.dimensions in self._vocabs and
+                    self._vocabs[vocab.dimensions] is vocab)
+
+
+class VocabularySetParam(nengo.params.Parameter):
+    """Can be a mapping from dimensions to vocabularies."""
+
+    def validate(self, instance, vocab_set):
+        super(VocabularySetParam, self).validate(instance, vocab_set)
+
+        if vocab_set is not None and not isinstance(vocab_set, VocabularySet):
+            raise ValidationError(
+                "Must be of type 'VocabularySet' (got type %r)."
+                % vocab_set.__class__.__name__, attr=self.name, obj=instance)
+
+        return vocab_set

nengo/nengo-spa

nengo_spa/__init__.py
@@ -7,9 +7,10 @@ from .compare import Compare
 from .cortical import Cortical
 from .input import Input
 from .memory import Memory
+from .module import Module
 from .pointer import SemanticPointer
 from .spa import SPA
 from .state import State
 from .thalamus import Thalamus
-from .utils import enable_spa_params, similarity
-from .vocab import Vocabulary
+from .utils import similarity
+from .vocab import Vocabulary, VocabularyMap

nengo_spa/spa.py
@@ -1,16 +1,10 @@
-import warnings
-
 import numpy as np
 
-import nengo
-from nengo.exceptions import SpaModuleError
-from nengo.spa.vocab import Vocabulary
+from nengo.spa.vocab import VocabularyMap
 from nengo.spa.module import Module
-from nengo.spa.utils import enable_spa_params
-from nengo.utils.compat import iteritems
 
 
-class SPA(nengo.Network):
+class SPA(Module):
     """Base class for SPA models.
 
     This expands the standard `.Network` system to support structured
@@ -80,153 +74,17 @@ class SPA(nengo.Network):
 
     def __init__(self, label=None, seed=None, add_to_container=None,
                  vocabs=None):
-        super(SPA, self).__init__(label, seed, add_to_container)
+        if seed is not None:
+            rng = np.random.RandomState(seed)
+        else:
+            rng = None
         vocabs = [] if vocabs is None else vocabs
-        enable_spa_params(self)
-        self._modules = {}
-        self._default_vocabs = {}
-
-        for vo in vocabs:
-            if vo.dimensions in self._default_vocabs:
-                warnings.warn("Duplicate vocabularies with dimension %d. "
-                              "Using the last entry in the vocab list with "
-                              "that dimensionality." % (vo.dimensions))
-            self._default_vocabs[vo.dimensions] = vo
-
-    def __setattr__(self, key, value):
-        """A setattr that handles Modules being added specially.
-
-        This is so that we can use the variable name for the Module as
-        the name that all of the SPA system will use to access that module.
-        """
-        if hasattr(self, key) and isinstance(getattr(self, key), Module):
-            raise SpaModuleError("Cannot re-assign module-attribute %s to %s. "
-                                 "SPA module-attributes can only be assigned "
-                                 "once." % (key, value))
-        super(SPA, self).__setattr__(key, value)
-        if isinstance(value, Module):
-            if value.label is None:
-                value.label = key
-            self._modules[key] = value
-            for k, (obj, v) in iteritems(value.inputs):
-                if type(v) == int:
-                    value.inputs[k] = (obj, self.get_default_vocab(v))
-                self.config[obj].vocab = value.inputs[k][1]
-            for k, (obj, v) in iteritems(value.outputs):
-                if type(v) == int:
-                    value.outputs[k] = (obj, self.get_default_vocab(v))
-                self.config[obj].vocab = value.outputs[k][1]
-
-            value.on_add(self)
-
-    def __exit__(self, ex_type, ex_value, traceback):
-        super(SPA, self).__exit__(ex_type, ex_value, traceback)
-        if ex_type is not None:
-            # re-raise the exception that triggered this __exit__
-            return False
-
-        module_list = frozenset(self._modules.values())
-        for net in self.networks:
-            # Since there are no attributes to distinguish what's been added
-            # and what hasn't, we have to ask the network
-            if isinstance(net, Module) and (net not in module_list):
-                raise SpaModuleError("%s must be set as an attribute of "
-                                     "a SPA network" % (net))
-
-    def get_module(self, name):
-        """Return the module for the given name."""
-        if name in self._modules:
-            return self._modules[name]
-        elif '_' in name:
-            module, name = name.rsplit('_', 1)
-            if module in self._modules:
-                return self._modules[module]
-        raise SpaModuleError("Could not find module %r" % name)
+        super(SPA, self).__init__(
+            label, seed, add_to_container, VocabularyMap(vocabs, rng))
 
     def get_default_vocab(self, dimensions):
         """Return a Vocabulary with the desired dimensions.
 
         This will create a new default Vocabulary if one doesn't exist.
         """
-
-        # If seed is set, create rng based off that seed.
-        # Otherwise, just use the default NumPy rng.
-        rng = None if self.seed is None else np.random.RandomState(self.seed)
-
-        if dimensions not in self._default_vocabs:
-            self._default_vocabs[dimensions] = Vocabulary(dimensions, rng=rng)
-        return self._default_vocabs[dimensions]
-
-    def get_module_input(self, name):
-        """Return the object to connect into for the given name.
-
-        The name will be either the same as a module, or of the form
-        ``<module_name>_<input_name>``.
-        """
-        if name in self._modules and 'default' in self._modules[name].inputs:
-            return self._modules[name].inputs['default']
-        elif '_' in name:
-            module, name = name.rsplit('_', 1)
-            if module in self._modules:
-                m = self._modules[module]
-                if name in m.inputs:
-                    return m.inputs[name]
-        raise SpaModuleError("Could not find module input %r" % name)
-
-    def get_module_inputs(self):
-        for name, module in iteritems(self._modules):
-            for input in module.inputs:
-                if input == 'default':
-                    yield name
-                else:
-                    yield '%s_%s' % (name, input)
-
-    def get_input_vocab(self, name):
-        return self.get_module_input(name)[1]
-
-    def get_module_output(self, name):
-        """Return the object to connect into for the given name.
-
-        The name will be either the same as a module, or of the form
-        ``<module_name>_<output_name>``.
-        """
-        if name in self._modules:
-            return self._modules[name].outputs['default']
-        elif '_' in name:
-            module, name = name.rsplit('_', 1)
-            if module in self._modules:
-                m = self._modules[module]
-                if name in m.outputs:
-                    return m.outputs[name]
-        raise SpaModuleError("Could not find module output %r" % name)
-
-    def get_module_outputs(self):
-        for name, module in iteritems(self._modules):
-            for output in module.outputs:
-                if output == 'default':
-                    yield name
-                else:
-                    yield '%s_%s' % (name, output)
-
-    def get_output_vocab(self, name):
-        return self.get_module_output(name)[1]
-
-    def similarity(self, data, probe, vocab=None):
-        """Return the similarity between the probed data and ``vocab``.
-
-        If no vocabulary is provided, the vocabulary associated with
-        ``probe.target`` will be used.
-
-        Parameters
-        ----------
-        data: ProbeDict
-            Collection of simulation data returned by sim.run() function call.
-        probe: Probe
-            Probe with desired data.
-        vocab : Vocabulary, optional (Default: None)
-            The vocabulary to compare with. If None, uses the vocabulary
-            associated with ``probe.target``.
-        """
-        if vocab is None:
-            vocab = self.config[probe.target].vocab
-        return nengo.spa.similarity(data[probe], vocab)
+        return self.vocabs.get_or_create(dimensions)

nengo_spa/tests/test_vocabulary.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 from nengo.exceptions import SpaParseError, ValidationError
-from nengo.spa import Vocabulary
+from nengo.spa import Vocabulary, VocabularyMap
 from nengo.utils.testing import warns
 
 
@@ -246,3 +246,47 @@ def test_extend(rng):
     v.extend(['G', 'H'], unitary=True)
     assert v.keys == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     assert v.unitary == ['E', 'G', 'H']
+
+
+def test_vocabulary_set(rng):
+    v8 = Vocabulary(8)
+    v16 = Vocabulary(16)
+    v32 = Vocabulary(32)
+    vs = VocabularyMap([v8, v16], rng=rng)
+
+    # Behaviour common to set and dict
+    assert len(vs) == 2
+    assert 8 in vs
+    assert 16 in vs
+    assert 32 not in vs
+
+    assert v8 in vs
+    assert v16 in vs
+    assert v32 not in vs
+    assert Vocabulary(8) not in vs
+
+    # dict behaviour
+    assert vs[8] is v8
+    assert vs[16] is v16
+
+    del vs[8]
+    assert 8 not in vs
+
+    # set behaviour
+    vs.add(v32)
+    assert vs[32] is v32
+    with pytest.warns(UserWarning):
+        vs.add(v32)
+
+    vs.discard(32)
+    assert 32 not in vs
+    vs.discard(v16)
+    assert 16 not in vs
+
+    # creating new vocabs if non existent
+    vs.add(v8)
+    assert vs.get_or_create(8) is v8
+    new = vs.get_or_create(16)
+    assert vs[16] is new
+    assert new.dimensions == 16
+    assert new.rng is rng

nengo_spa/vocab.py
@@ -1,3 +1,4 @@
+from collections import Mapping
 import warnings
 
 import numpy as np
@@ -480,15 +481,64 @@ class Vocabulary(object):
         return subset
 
 
-class VocabularyParam(nengo.params.Parameter):
-    """Can be a Vocabulary."""
+class VocabularyMap(Mapping):
+    """Maps dimensionalities to corresponding vocabularies."""
+    def __init__(self, vocabs=None, rng=None):
+        if vocabs is None:
+            vocabs = []
+        self.rng = rng
 
-    def validate(self, instance, vocab):
-        super(VocabularyParam, self).validate(instance, vocab)
+        self._vocabs = {}
+        for vo in vocabs:
+            self.add(vo)
 
-        if vocab is not None and not isinstance(vocab, Vocabulary):
-            raise ValidationError("Must be of type 'Vocabulary' (got type %r)."
-                                  % type(vocab).__name__,
-                                  attr=self.name, obj=instance)
+    def add(self, vocab):
+        if vocab.dimensions in self._vocabs:
+            warnings.warn("Duplicate vocabularies with dimension %d. "
+                          "Using the last entry in the vocab list with "
+                          "that dimensionality." % (vocab.dimensions))
+        self._vocabs[vocab.dimensions] = vocab
 
-        return vocab
+    def __delitem__(self, dimensions):
+        del self._vocabs[dimensions]
+
+    def discard(self, vocab):
+        if isinstance(vocab, int):
+            del self._vocabs[vocab]
+        elif self._vocabs.get(vocab.dimensions, None) is vocab:
+            del self._vocabs[vocab.dimensions]
+
+    def __getitem__(self, dimensions):
+        return self._vocabs[dimensions]
+
+    def get_or_create(self, dimensions):
+        if dimensions not in self._vocabs:
+            self._vocabs[dimensions] = Vocabulary(dimensions, rng=self.rng)
+        return self._vocabs[dimensions]
+
+    def __iter__(self):
+        return iter(self._vocabs)
+
+    def __len__(self):
+        return len(self._vocabs)
+
+    def __contains__(self, vocab):
+        if isinstance(vocab, int):
+            return vocab in self._vocabs
+        else:
+            return (vocab.dimensions in self._vocabs and
+                    self._vocabs[vocab.dimensions] is vocab)
+
+
+class VocabularyMapParam(nengo.params.Parameter):
+    """Can be a mapping from dimensions to vocabularies."""
+
+    def validate(self, instance, vocab_set):
+        super(VocabularyMapParam, self).validate(instance, vocab_set)
+
+        if vocab_set is not None and not isinstance(vocab_set, VocabularyMap):
+            raise ValidationError(
+                "Must be of type 'VocabularyMap' (got type %r)."
+                % type(vocab_set).__name__, attr=self.name, obj=instance)
+
+        return vocab_set