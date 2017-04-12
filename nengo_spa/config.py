a3572c6e6ba255b2ec291c4563aa750a51bac7c6
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-04-12 17:18:57 -0400

    Add missing classes.
    Copied from the Nengo branch.

---
+++ nengo_spa/config.py
@@ -0,0 +1,19 @@
+from nengo.config import Config
+from nengo.params import Parameter
+
+
+class ConfigParam(Parameter):
+    """A parameter where the value is a `.Config` object."""
+
+    def validate(self, instance, value):
+        super(ConfigParam, self).validate(instance, value)
+
+        if value is not None and not isinstance(value, Config):
+            raise ValidationError(
+                "Must be of type 'Config' (got type %r)."
+                % type(value).__name__, attr=self.name, obj=instance)
+
+    def __set__(self, instance, value):
+        if value is None:
+            value = Config()
+        super(ConfigParam, self).__set__(instance, value)
---
+++ nengo_spa/exceptions.py
@@ -0,0 +1,14 @@
+class SpaException(Exception):
+    """A exception within the SPA subsystem."""
+
+
+class SpaModuleError(SpaException, ValueError):
+    """An error in how SPA keeps track of modules."""
+
+
+class SpaParseError(SpaException, ValueError):
+    """An error encountered while parsing a SPA expression."""
+
+
+class SpaTypeError(SpaException, ValueError):
+    """The evaluation of types in an SPA expression was invalid."""
