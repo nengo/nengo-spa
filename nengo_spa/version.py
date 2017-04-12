7d15f3c2a9e693e157d5c45671b3c4daf9f252be
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-04-12 16:01:28 -0400

    Add version information.
    Basic code structure has been copied from Nengo.

---
+++ nengo_spa/version.py
@@ -0,0 +1,14 @@
+"""Nengo version information.
+
+We use semantic versioning (see http://semver.org/).
+and conform to PEP440 (see https://www.python.org/dev/peps/pep-0440/).
+'.devN' will be added to the version unless the code base represents
+a release version. Release versions are git tagged with the version.
+"""
+
+name = "nengo_spa"
+version_info = (0, 0, 1)  # (major, minor, patch)
+dev = 0
+
+version = "{v}{dev}".format(v=".".join(str(v) for v in version_info),
+                            dev="" if dev is None else ".dev{:d}".format(dev))
