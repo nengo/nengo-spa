tisana69/nengo

Add subclasses for different assoc mem variants.

nengo/spa/assoc_mem.py
@@ -1,7 +1,7 @@
 import nengo
 import numpy as np
 from nengo.spa.module import Module
-from nengo.spa.selection import ThresholdingArray
+from nengo.spa.selection import ThresholdingArray, WTA
 from nengo.spa.vocab import VocabularyOrDimParam
 from nengo.utils.network import with_self
 
@@ -73,3 +73,33 @@ class AssociativeMemory(Module):
             self.selection.output, self.default_ens,
             transform=-np.ones(
                 (1, self.selection.output.size_out)) / min_activation_value)
+
+
+class ThresholdingAssocMem(AssociativeMemory):
+    def __init__(
+            self, threshold, input_vocab, output_vocab=None, input_keys=None,
+            output_keys=None, n_neurons=50, label=None, seed=None,
+            add_to_container=None, vocabs=None, **selection_net_args):
+        selection_net_args['threshold'] = threshold
+        super(ThresholdingAssocMem, self).__init__(
+            selection_net=ThresholdingArray,
+            input_vocab=input_vocab, output_vocab=output_vocab,
+            input_keys=input_keys, output_keys=output_keys,
+            n_neurons=n_neurons, label=label, seed=seed,
+            add_to_container=add_to_container, vocabs=None,
+            **selection_net_args)
+
+
+class WtaAssocMem(AssociativeMemory):
+    def __init__(
+            self, threshold, input_vocab, output_vocab=None, input_keys=None,
+            output_keys=None, n_neurons=50, label=None, seed=None,
+            add_to_container=None, vocabs=None, **selection_net_args):
+        selection_net_args['threshold'] = threshold
+        super(WtaAssocMem, self).__init__(
+            selection_net=WTA,
+            input_vocab=input_vocab, output_vocab=output_vocab,
+            input_keys=input_keys, output_keys=output_keys,
+            n_neurons=n_neurons, label=label, seed=seed,
+            add_to_container=add_to_container, vocabs=None,
+            **selection_net_args)

nengo/nengo-spa

nengo_spa/assoc_mem.py
@@ -1,7 +1,7 @@
 import nengo
 import numpy as np
 from nengo_spa.module import Module
-from nengo_spa.selection import ThresholdingArray
+from nengo_spa.selection import ThresholdingArray, WTA
 from nengo_spa.vocab import VocabularyOrDimParam
 from nengo.utils.network import with_self
 
@@ -73,3 +73,33 @@ class AssociativeMemory(Module):
             self.selection.output, self.default_ens,
             transform=-np.ones(
                 (1, self.selection.output.size_out)) / min_activation_value)
+
+
+class ThresholdingAssocMem(AssociativeMemory):
+    def __init__(
+            self, threshold, input_vocab, output_vocab=None, input_keys=None,
+            output_keys=None, n_neurons=50, label=None, seed=None,
+            add_to_container=None, vocabs=None, **selection_net_args):
+        selection_net_args['threshold'] = threshold
+        super(ThresholdingAssocMem, self).__init__(
+            selection_net=ThresholdingArray,
+            input_vocab=input_vocab, output_vocab=output_vocab,
+            input_keys=input_keys, output_keys=output_keys,
+            n_neurons=n_neurons, label=label, seed=seed,
+            add_to_container=add_to_container, vocabs=vocabs,
+            **selection_net_args)
+
+
+class WtaAssocMem(AssociativeMemory):
+    def __init__(
+            self, threshold, input_vocab, output_vocab=None, input_keys=None,
+            output_keys=None, n_neurons=50, label=None, seed=None,
+            add_to_container=None, vocabs=None, **selection_net_args):
+        selection_net_args['threshold'] = threshold
+        super(WtaAssocMem, self).__init__(
+            selection_net=WTA,
+            input_vocab=input_vocab, output_vocab=output_vocab,
+            input_keys=input_keys, output_keys=output_keys,
+            n_neurons=n_neurons, label=label, seed=seed,
+            add_to_container=add_to_container, vocabs=vocabs,
+            **selection_net_args)