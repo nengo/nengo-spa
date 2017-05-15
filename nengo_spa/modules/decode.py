a3d96b58d6c023fbbabab43e2f59ef837a5627b3
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-05-15 15:45:52 -0400

    Refactor Encode, Decode, Transcode.

--- nengo_spa/modules/decode.py
+++ nengo_spa/modules/decode.py
@@ -1,37 +1,16 @@
 import nengo
 from nengo.config import Default
-from nengo.exceptions import ValidationError
-from nengo.params import IntParam, Parameter
-from nengo.utils.stdlib import checked_call
+from nengo.params import IntParam
 
+from nengo_spa.modules.transcode import TranscodeFunctionParam
 from nengo_spa.network import Network
-from nengo_spa.pointer import SemanticPointer
 from nengo_spa.vocab import VocabularyOrDimParam
 
 
-def make_sp_func(fn, vocab):
-    def sp_func(t, v):
-        return fn(t, SemanticPointer(v), vocab)
-    return sp_func
-
-
-class DecodeFunctionParam(Parameter):
-    def coerce(self, obj, value):
-        if not callable(value):
-            raise ValidationError("Not callable.", attr=self.name, obj=obj)
-        _, invoked = checked_call(
-            value, 0., SemanticPointer(obj.vocab.dimensions), obj.vocab)
-        if not invoked:
-            raise ValidationError(
-                "Decode function %r is expected to accept exactly 3 "
-                "arguments: time as a float, a SemanticPointer, and a "
-                "Vocabulary.", attr=self.name, obj=node)
-        return super(DecodeFunctionParam, self).coerce(obj, value)
-
-
 class Decode(Network):
-    function = DecodeFunctionParam(
-        'function', optional=False, default=None, readonly=True)
+    function = TranscodeFunctionParam(
+        'function', sp_output=False, optional=False, default=None,
+        readonly=True)
     vocab = VocabularyOrDimParam(
         'vocab', optional=False, default=None, readonly=True)
     size_out = IntParam(
@@ -48,10 +27,15 @@ class Decode(Network):
 
         with self:
             self.node = nengo.Node(
-                make_sp_func(self.function, self.vocab),
+                TranscodeFunctionParam.to_node_output(
+                    self.function, input_vocab=self.vocab),
                 size_in=self.vocab.dimensions, size_out=self.size_out)
             self.input = self.node
             self.output = self.node
 
         self.inputs = dict(default=(self.input, self.vocab))
         self.outputs = dict()
+
+    @property
+    def input_vocab(self):
+        return self.vocab
--- nengo_spa/modules/encode.py
+++ nengo_spa/modules/encode.py
@@ -1,93 +1,44 @@
 import nengo
 from nengo.config import Default
-from nengo.exceptions import ValidationError
-from nengo.params import IntParam, Parameter
-from nengo.utils.compat import is_string
+from nengo.params import IntParam
 
+from nengo_spa.modules.transcode import TranscodeFunctionParam
 from nengo_spa.network import Network
-from nengo_spa.pointer import SemanticPointer
 from nengo_spa.vocab import VocabularyOrDimParam
-
-
-class SpArrayExtractor(object):
-    # TODO is there a better place for this class?
-    def __init__(self, vocab):
-        self.vocab = vocab
-
-    def __call__(self, value):
-        if is_string(value):
-            value = self.vocab.parse(value)
-        if isinstance(value, SemanticPointer):
-            value = value.v
-        return value
-
-
-def make_parse_func(fn, vocab):
-    """Create a function that calls func and parses the output in vocab."""
-
-    extractor = SpArrayExtractor(vocab)
-
-    def parse_func(*args):
-        return extractor(fn(*args))
-
-    return parse_func
-
-
-class EncodeFunctionParam(Parameter):
-    def coerce(self, node, output):
-        output = super(EncodeFunctionParam, self).coerce(node, output)
-
-        if output is None:
-            return output
-        elif isinstance(output, nengo.Process):
-            raise NotImplementedError()
-        elif callable(output):
-            return output
-        elif is_string(output):
-            return output
-        else:
-            raise ValidationError("Invalid output type {!r}".format(
-                type(output)), attr=self.name, obj=node)
-
-    @classmethod
-    def to_vector_output(cls, output, vocab):
-        if output is None:
-            return None
-        elif isinstance(output, nengo.Process):
-            raise NotImplementedError()
-        elif callable(output):
-            return make_parse_func(output, vocab)
-        elif is_string(output):
-            return SpArrayExtractor(vocab)(output)
-        else:
-            raise ValueError("Invalid output type {!r}".format(type(output)))
 
 
 class Encode(Network):
     """A SPA network for providing external inputs to other networks."""
 
-    function = EncodeFunctionParam(
-        'function', optional=False, default=None, readonly=True)
+    function = TranscodeFunctionParam(
+        'function', sp_input=False, optional=False, default=None,
+        readonly=True)
     vocab = VocabularyOrDimParam(
         'vocab', optional=False, default=None, readonly=True)
     size_in = IntParam(
         'size_in', low=0, optional=True, default=None, readonly=True)
 
     def __init__(
-            self, sp_output=Default, vocab=Default, size_in=Default, **kwargs):
+            self, function=Default, vocab=Default, size_in=Default, **kwargs):
         super(Encode, self).__init__(**kwargs)
 
-        self.function = sp_output
+        # Vocab and size_in need to be set before function which accesses these
+        # for validation.
         self.vocab = vocab
         self.size_in = size_in
+        self.function = function
 
         with self:
             self.node = nengo.Node(
-                EncodeFunctionParam.to_vector_output(
-                    self.function, self.vocab),
+                TranscodeFunctionParam.to_node_output(
+                    self.function, output_vocab=self.vocab),
                 size_in=self.size_in, size_out=self.vocab.dimensions)
             self.input = self.node
             self.output = self.node
 
         self.inputs = dict()
         self.outputs = dict(default=(self.output, self.vocab))
+
+    @property
+    def output_vocab(self):
+        return self.vocab
