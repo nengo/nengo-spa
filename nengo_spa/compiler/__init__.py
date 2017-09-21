86f4d820cda3e90f4d17a24b03f931337e31fc9b
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-09-21 14:23:55 -0400

    Add wrapper for Python tokenize module.

---
+++ nengo_spa/compiler/__init__.py
@@ -0,0 +1,6 @@
+"""This package implements the compilation of Nengo SPA action rules.
+
+.. warning:
+   Objects in this package are not part of the public Nengo SPA API. Anything
+   in this package might change between Nengo SPA releases without notice.
+"""
