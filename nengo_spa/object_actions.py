1d45012ea65c081af45285ce54048c422c53f0f3
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2017-11-09 14:02:00 -0500

    Add object-based Action interface

---
+++ nengo_spa/object_actions.py
@@ -0,0 +1,98 @@
+from nengo.network import Network
+from nengo.exceptions import NetworkContextError
+from nengo_spa.actions import AstAccessor
+from nengo_spa.compiler import ast_nodes as nodes
+from nengo_spa.compiler.ast import ConstructionContext
+
+
+def to_node(obj, as_sink=False):
+    if isinstance(obj, nodes.Node):
+        return obj
+    elif obj is None or obj == "0":
+        return nodes.Zero()
+    elif obj == "1":
+        return nodes.One()
+    elif isinstance(obj, str):
+        return nodes.Symbol(obj)
+    elif isinstance(obj, (int, float)):
+        return nodes.Scalar(obj)
+    else:
+        name = str(obj)
+        return (nodes.Sink(name, obj) if as_sink else
+                nodes.Module(name, obj))
+
+
+def dot(a, b):
+    return nodes.DotProduct(to_node(a), to_node(b))
+
+
+def bind(a, b):
+    return nodes.Product(to_node(a), to_node(b))
+
+
+def add(a, b):
+    return nodes.Sum(to_node(a), to_node(b))
+
+
+def inv(a):
+    return nodes.ApproxInverse(to_node(a))
+
+
+def neg(a):
+    return nodes.Negative(to_node(a))
+
+
+def route(a, b):
+    return nodes.Effect(to_node(b, as_sink=True), to_node(a))
+
+
+def reinterpret(a):
+    return nodes.Reinterpret(a)
+
+
+def translate(a):
+    return nodes.Translate(a)
+
+
+class Actions(object):
+    def __init__(self, *blocks):
+        self.blocks = []
+        self.data = AstAccessor(self.blocks)
+        for b in blocks:
+            self.add_block(b)
+
+    def add_block(self, actions):
+        if len(Network.context) <= 0:
+            raise NetworkContextError(
+                "actions can only be called inside a ``with network:`` block.")
+        root_network = Network.context[-1]
+
+        if not isinstance(actions, (list, tuple)):
+            actions = [actions]
+
+        if isinstance(actions[0], (list, tuple)):
+            blocks = self._add_bg_block(actions)
+        else:
+            blocks = self._add_cortical_block(actions)
+
+        for block in blocks:
+            block.infer_types(None)
+        construction_context = ConstructionContext(root_network)
+        for block in blocks:
+            block.construct(construction_context)
+        self.blocks += blocks
+
+    def _add_cortical_block(self, effects):
+        return [to_node(e) for e in effects]
+
+    def _add_bg_block(self, actions):
+        action_nodes = []
+        for i, (cond, effects) in enumerate(actions):
+            if not isinstance(effects, (tuple, list)):
+                effects = [effects]
+            effects = nodes.Effects([to_node(e) for e in effects])
+            for e in effects.effects:
+                e.channeled = True
+            action_nodes.append(
+                nodes.Action(to_node(cond), effects, index=i))
+        return [nodes.ActionSet(action_nodes)]
---
+++ nengo_spa/tests/test_object_actions.py
@@ -0,0 +1,292 @@
+import numpy as np
+import pytest
+
+import nengo
+import nengo_spa as spa
+from nengo_spa.exceptions import SpaTypeError
+from nengo_spa import object_actions as oact
+
+
+def test_new_action_syntax(Simulator, seed, plt, rng):
+    model = spa.Network(seed=seed)
+    model.config[spa.State].vocab = 3
+    model.config[spa.State].subdimensions = 3
+    with model:
+        model.ctrl = spa.State(16, subdimensions=16, label='ctrl')
+
+        def input_func(t):
+            if t < 0.2:
+                return 'A'
+            elif t < 0.4:
+                return 'B'
+            else:
+                return 'C'
+
+        model.input = spa.Transcode(input_func, output_vocab=16)
+
+        model.state = spa.State(label='state')
+        model.buff1 = spa.State(label='buff1')
+        model.buff2 = spa.State(label='buff2')
+        model.buff3 = spa.State(label='buff3')
+
+        node1 = nengo.Node([0, 1, 0])
+        node2 = nengo.Node([0, 0, 1])
+
+        nengo.Connection(node1, model.buff1.input)
+        nengo.Connection(node2, model.buff2.input)
+
+        # spa.Actions('''
+        #     model.input -> model.ctrl
+        #     model.buff1 -> model.state
+        #     ifmax dot(model.ctrl, A):
+        #         model.buff1 -> model.buff3
+        #     elifmax dot(model.ctrl, B):
+        #         model.buff2 -> model.buff3
+        #     elifmax dot(model.ctrl, C):
+        #         model.buff1 * model.buff2 -> model.buff3
+        # ''')
+        actions = oact.Actions()
+        actions.add_block([oact.route(model.input, model.ctrl),
+                           oact.route(model.buff1, model.state)])
+        actions.add_block([
+            (oact.dot(model.ctrl, "A"), oact.route(model.buff1, model.buff3)),
+            (oact.dot(model.ctrl, "B"), oact.route(model.buff2, model.buff3)),
+            (oact.dot(model.ctrl, "C"), oact.route(
+                oact.bind(model.buff1, model.buff2), model.buff3))
+        ])
+
+        state_probe = nengo.Probe(model.state.output, synapse=0.03)
+        buff3_probe = nengo.Probe(model.buff3.output, synapse=0.03)
+
+    with Simulator(model) as sim:
+        sim.run(0.6)
+
+    data = sim.data[buff3_probe]
+
+    plt.plot(sim.trange(), data)
+
+    state_val = np.mean(sim.data[state_probe][200:], axis=0)
+    assert state_val[0] < 0.2
+    assert state_val[1] > 0.8
+    assert state_val[2] < 0.2
+
+    valueA = np.mean(data[150:200], axis=0)  # should be [0, 1, 0]
+    valueB = np.mean(data[350:400], axis=0)  # should be [0, 0, 1]
+    valueC = np.mean(data[550:600], axis=0)  # should be [1, 0, 0]
+
+    assert valueA[0] < 0.2
+    assert valueA[1] > 0.75
+    assert valueA[2] < 0.2
+
+    assert valueB[0] < 0.2
+    assert valueB[1] < 0.2
+    assert valueB[2] > 0.75
+
+    assert valueC[0] > 0.75
+    assert valueC[1] < 0.2
+    assert valueC[2] < 0.2
+
+
+def test_dot_product(Simulator, seed, plt):
+    d = 16
+
+    with spa.Network(seed=seed) as model:
+        model.state_a = spa.State(d)
+        model.state_b = spa.State(d)
+        model.result = spa.Scalar()
+
+        model.stimulus = spa.Transcode(
+            lambda t: 'A' if t <= 0.3 else 'B', output_vocab=d)
+
+        # spa.Actions('''
+        #     A -> model.state_a
+        #     model.stimulus -> model.state_b
+        #     dot(model.state_a, model.state_b) -> model.result
+        # ''')
+        oact.Actions([
+            oact.route("A", model.state_a),
+            oact.route(model.stimulus, model.state_b),
+            oact.route(oact.dot(model.state_a, model.state_b), model.result)])
+        p = nengo.Probe(model.result.output, synapse=0.03)
+
+    with Simulator(model) as sim:
+        sim.run(0.6)
+
+    plt.plot(sim.trange(), sim.data[p])
+
+    t = sim.trange()
+    assert np.mean(sim.data[p][(t > 0.1) & (t <= 0.3)]) > 0.8
+    assert np.mean(sim.data[p][t > 0.4]) < 0.15
+
+
+class TestExceptions():
+    @pytest.fixture
+    def model(self):
+        with spa.Network() as model:
+            model.state_a = spa.State(16)
+            model.state_b = spa.State(32)
+            model.state_c = spa.State(32)
+            model.scalar = spa.Scalar()
+        return model
+
+    def test_invalid_types_in_binary_operation(self, model):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions('model.state_a + model.state_b -> model.state_c')
+                oact.Actions(oact.route(oact.add(model.state_a, model.state_b),
+                                        model.state_c))
+
+    def test_approx_inverse_of_scalar(self, model):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions('~model.scalar -> model.state_c')
+                oact.Actions(oact.route(model.scalar, model.state_c))
+
+    def test_dot_product_incompatiple_vocabs(self, model):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions(
+                #     'dot(model.state_a, model.state_b) -> model.scalar')
+                oact.Actions(
+                    oact.route(oact.dot(model.state_a, model.state_b),
+                               model.scalar))
+
+    def test_dot_product_first_argument_invalid(self, model):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions(
+                #     'dot(model.scalar, model.state_b) -> model.scalar')
+                oact.Actions(
+                    oact.route(oact.dot(model.scalar, model.state_b),
+                               model.scalar))
+
+    def test_dot_product_second_argument_invalid(self, model):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions(
+                #     'dot(model.state_a, model.scalar) -> model.scalar')
+                oact.Actions(
+                    oact.route(oact.dot(model.state_a, model.scalar),
+                               model.scalar))
+
+    @pytest.mark.parametrize('method', ['reinterpret', 'translate'])
+    def test_cast_type_inference_not_possible(self, model, method):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions('dot({}(model.state_a), A) -> model.scalar'.format(
+                #     method))
+                method = getattr(oact, method)
+                oact.Actions(oact.route(oact.dot(method(model.state_a), "A"),
+                                        model.scalar))
+
+    @pytest.mark.parametrize('method', ['reinterpret', 'translate'])
+    def test_cast_scalar(self, model, method):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions(
+                #     '{}(model.scalar) -> model.state_a'.format(method))
+                method = getattr(oact, method)
+                oact.Actions(oact.route(method(model.scalar), model.state_a))
+
+    def test_reinterpret_non_matching_dimensions(self, model):
+        with model:
+            with pytest.raises(SpaTypeError):
+                # spa.Actions('reinterpret(model.state_b) -> model.state_a')
+                oact.Actions(oact.route(oact.reinterpret(model.state_b),
+                                        model.state_a))
+
+
+def test_access_actions():
+    d = 16
+    with spa.Network() as m:
+        m.a = spa.State(d)
+        m.b = spa.State(d)
+        m.c = spa.State(d)
+        m.d = spa.State(d)
+        # actions = spa.Actions('''
+        #     m.b -> m.a
+        #     m.c -> m.b
+        #     always as 'named':
+        #         m.d -> m.c
+        #     ''')
+        actions = oact.Actions([
+            oact.route(m.b, m.a),
+            oact.route(m.c, m.b),
+            oact.route(m.d, m.c)]).data
+
+    assert len(actions) == 3
+    # assert str(actions[0]) == 'm.b -> m.a'
+    assert str(actions[0]) == "%s -> %s" % (m.b, m.a)
+    # assert str(actions[1]) == 'm.c -> m.b'
+    assert str(actions[1]) == "%s -> %s" % (m.c, m.b)
+
+    # this is being removed
+    # https://github.com/nengo/nengo_spa/pull/100
+    # assert str(actions[2]) == '''always as 'named':
+    # m.d -> m.c'''
+    # assert str(actions['named']) == '''always as 'named':
+    # m.d -> m.c'''
+
+
+def test_access_thal_and_bg_objects():
+    d = 16
+
+    with spa.Network() as m:
+        m.a = spa.Scalar()
+        m.b = spa.Scalar()
+
+        m.c = spa.Scalar()
+        m.d = spa.Scalar()
+
+        # actions = spa.Actions('''
+        #     ifmax m.a:
+        #         0 -> m.c
+        #     ifmax m.b:
+        #         1 -> m.c
+        #     m.c -> m.d
+        #     ''')
+        actions = oact.Actions(
+            [(m.a, oact.route("0", m.c))],
+            [(m.b, oact.route("1", m.c))],
+            oact.route(m.c, m.d)).data
+
+    assert actions.all_bgs() == [actions[0].bg, actions[1].bg]
+    assert actions.all_thals() == [actions[0].thalamus, actions[1].thalamus]
+
+    with spa.Network() as m:
+        m.a = spa.State(d)
+        m.b = spa.State(d)
+
+        # actions = spa.Actions('''
+        #     m.a -> m.b
+        #     ''')
+        actions = oact.Actions(oact.route(m.a, m.b)).data
+
+    assert len(actions.all_bgs()) == 0
+    assert len(actions.all_thals()) == 0
+
+
+def test_provides_access_to_constructed_objects_of_effect():
+    with spa.Network() as model:
+        model.config[spa.State].vocab = 16
+        model.a = spa.State()
+        model.b = spa.State()
+        model.c = spa.State()
+
+        # actions = spa.Actions('model.a * model.b -> model.c')
+        actions = oact.Actions(oact.route(oact.bind(model.a, model.b),
+                                          model.c)).data
+
+        assert len(actions[0].constructed) == 1
+        assert isinstance(actions[0].constructed[0], nengo.Connection)
+        assert len(actions[0].source.constructed) == 3
+        n_connections = 0
+        n_bind = 0
+        for obj in actions[0].source.constructed:
+            if isinstance(obj, nengo.Connection):
+                n_connections += 1
+            elif isinstance(obj, spa.Bind):
+                n_bind += 1
+            else:
+                raise AssertionError("Unexpected object constructed for Bind.")
+        assert n_connections == 2 and n_bind == 1
