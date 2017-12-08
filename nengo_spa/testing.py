bfdb1a8b216793469cb1820e51ca283d0c6399ab
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-12-08 17:49:27 -0500

    More tests.

--- nengo_spa/testing.py
+++ nengo_spa/testing.py
@@ -2,9 +2,14 @@ import numpy as np
 
 
 def sp_close(
-        t, data, target_sp, skip=0., duration=None, atol=0.2):
+        t, data, target_sp, skip=0., duration=None, atol=0.2,
+        normalized=False):
     if duration is None:
         duration = np.max(t) - skip
+    actual = data[(skip < t) & (t <= skip + duration)]
+    expected = target_sp
+    if normalized:
+        actual /= np.linalg.norm(actual, axis=1, keepdims=True)
+        expected = expected.normalized()
     return np.all(np.sqrt(np.sum(
-        np.square(data[(skip < t) & (t <= skip + duration)] - target_sp.v),
-        axis=1)) < atol)
+        np.square(actual - expected.v), axis=1)) < atol)
