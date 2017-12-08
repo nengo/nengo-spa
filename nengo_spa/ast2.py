e0bd7c1aab7eac3f9409be0d79c3d95e87965ac0
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-12-08 16:00:09 -0500

    Cleanup and simplify new AST code.

--- nengo_spa/ast2.py
+++ nengo_spa/ast2.py
@@ -33,36 +33,13 @@ def coerce_types(*types):
 
 def as_node(obj):
     if is_number(obj):
-        obj = Scalar(obj)
+        obj = FixedScalar(obj)
     return obj
 
 
 class Node(object):
-    class Staticity:
-        """Valid staticity values.
-
-        * ``FIXED``: Value of the node is static, i.e. does not change over
-          time.
-        * ``TRANSFORM_ONLY``: Value of the node changes over time, but can be
-          implemented with a transform on existing neural resources.
-        * ``DYNAMIC``: Value of the node is fully dynamic and needs additional
-          neural resources to be implemented.
-        """
-        FIXED = 0
-        TRANSFORM_ONLY = 1
-        DYNAMIC = 2
-
-    def __init__(self, staticity):
-        self.staticity = staticity
-        self.type = None
-
-    @property
-    def fixed(self):
-        """Indicates whether the node value is static.
-
-        A static node value does not change over time.
-        """
-        return self.staticity <= self.Staticity.FIXED
+    def __init__(self, type_=None):
+        self.type = type_
 
     def infer_types(self, context_type):
         pass
@@ -74,15 +51,19 @@ class Node(object):
         raise NotImplementedError()
 
 
-class Scalar(Node):
-    def __init__(self, value):
-        super(Scalar, self).__init__(self.Staticity.FIXED)
-        self.value = value
-        self.type = TScalar
+class FixedNode(Node):
+    def evaluate(self):
+        raise NotImplementedError()
 
     @property
     def expr(self):
-        return repr(self.value)
+        raise NotImplementedError()
+
+
+class FixedScalar(FixedNode):
+    def __init__(self, value):
+        super(FixedScalar, self).__init__(type_=TScalar)
+        self.value = value
 
     def connect_to(self, sink):
         return nengo.Connection(self.construct(), sink)
@@ -90,25 +71,35 @@ class Scalar(Node):
     def construct(self):
         return nengo.Node(self.value, label=str(self.value))
 
+    def evaluate(self):
+        return self.value
 
-class FixedPointer(Node):
+    @property
+    def expr(self):
+        return repr(self.value)
+
+
+class FixedPointer(FixedNode):
     def __init__(self, expr, type_=None):
-        super(FixedPointer, self).__init__(self.Staticity.FIXED)
-        self.expr = expr
-        self.type = type_
+        super(FixedPointer, self).__init__(type_=type_)
+        self._expr = expr
 
     def infer_types(self, type_):
         if self.type is None:
             self.type = type_
-
-    def evaluate(self):
-        return self.type.vocab.parse(self.expr)
 
     def connect_to(self, sink):
         return nengo.Connection(self.construct(), sink)
 
     def construct(self):
         return nengo.Node(self.evaluate().v, label=self.expr)
+
+    def evaluate(self):
+        return self.type.vocab.parse(self.expr)
+
+    @property
+    def expr(self):
+        return self._expr
 
     def __invert__(self):
         return FixedPointer('~' + self.expr, self.type)
@@ -118,7 +109,7 @@ class FixedPointer(Node):
 
     def __add__(self, other):
         other = as_node(other)
-        if not isinstance(other, (FixedPointer, Scalar)):
+        if not isinstance(other, FixedNode):
             return NotImplemented
         type_ = coerce_types(self.type, other.type)
         return FixedPointer(self.expr + '+' + other.expr, type_)
@@ -128,7 +119,7 @@ class FixedPointer(Node):
 
     def __sub__(self, other):
         other = as_node(other)
-        if not isinstance(other, (FixedPointer, Scalar)):
+        if not isinstance(other, FixedNode):
             return NotImplemented
         type_ = coerce_types(self.type, other.type)
         return FixedPointer(self.expr + '-' + other.expr, type_)
@@ -138,14 +129,14 @@ class FixedPointer(Node):
 
     def __mul__(self, other):
         other = as_node(other)
-        if not isinstance(other, (FixedPointer, Scalar)):
+        if not isinstance(other, FixedNode):
             return NotImplemented
         type_ = coerce_types(self.type, other.type)
         return FixedPointer(self.expr + '*' + other.expr, type_)
 
     def __rmul__(self, other):
         other = as_node(other)
-        if not isinstance(other, (FixedPointer, Scalar)):
+        if not isinstance(other, FixedNode):
             return NotImplemented
         type_ = coerce_types(self.type, other.type)
         return FixedPointer(other.expr + '*' + self.expr, type_)
@@ -155,9 +146,6 @@ class FixedPointer(Node):
 
 
 class DynamicNode(Node):
-    def __init__(self):
-        super(DynamicNode, self).__init__(self.Staticity.DYNAMIC)
-
     def __invert__(self):
         # FIXME alternate binding operators
         vocab = self.type.vocab
@@ -189,7 +177,7 @@ class DynamicNode(Node):
         other = as_node(other)
         other.infer_types(self.type)
 
-        if other.fixed:
+        if isinstance(other, FixedNode):
             # FIXME check AST type or instance type?
             if other.type == TScalar:
                 tr = other.value
@@ -215,7 +203,7 @@ class DynamicNode(Node):
         other = as_node(other)
         other.infer_types(self.type)
 
-        if other.fixed:
+        if isinstance(other, FixedNode):
             # FIXME check AST type or instance type?
             if other.type == TScalar:
                 tr = other.value
@@ -241,10 +229,9 @@ class DynamicNode(Node):
 
 class Transformed(DynamicNode):
     def __init__(self, source, transform, type_):
-        super(Transformed, self).__init__()
+        super(Transformed, self).__init__(type_=type_)
         self.source = source
         self.transform = transform
-        self.type = type_
 
     def connect_to(self, sink):
         # FIXME connection params
@@ -258,9 +245,8 @@ class Transformed(DynamicNode):
 
 class Summed(DynamicNode):
     def __init__(self, sources, type_):
-        super(Summed, self).__init__()
+        super(Summed, self).__init__(type_=type_)
         self.sources = sources
-        self.type = type_
 
     def infer_types(self, type_):
         for s in self.sources:
@@ -278,9 +264,8 @@ class Summed(DynamicNode):
 
 class ModuleOutput(DynamicNode):
     def __init__(self, output, type_):
-        super(ModuleOutput, self).__init__()
+        super(ModuleOutput, self).__init__(type_=type_)
         self.output = output
-        self.type = type_
 
     def construct(self):
         return self.output
