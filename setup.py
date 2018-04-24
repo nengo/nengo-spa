d068a655157c9f375f3d7815177529973d557936
Sean Aubin <seanaubin@gmail.com>
2018-04-24 13:14:13 -0400

    Fix up setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ except ImportError:
 
 
 def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
+    encoding = kwargs.get("encoding", "utf-8")
+    sep = kwargs.get("sep", "\n")
     buf = []
     for filename in filenames:
         with io.open(filename, encoding=encoding) as f:
@@ -25,48 +25,48 @@ def read(*filenames, **kwargs):
 
 root = os.path.dirname(os.path.realpath(__file__))
 version_module = imp.load_source(
-    'version', os.path.join(root, 'nengo_spa', 'version.py'))
-testing = 'test' in sys.argv or 'pytest' in sys.argv
+    "version", os.path.join(root, "nengo_spa", "version.py"))
+testing = "test" in sys.argv or "pytest" in sys.argv
 
 setup(
     name="nengo_spa",
     version=version_module.version,
     author="Applied Brain Research",
     author_email="info@appliedbrainresearch.com",
-    packages=find_packages(exclude=['*.tests']),
+    packages=find_packages(exclude=["*.tests"]),
     scripts=[],
     license="Free for non-commercial use",
     description="An implementation of the Semantic Pointer Architecture for "
                 "Nengo",
-    long_description=read('README.rst', 'CHANGES.rst'),
+    long_description=read("README.rst", "CHANGES.rst"),
     zip_safe=True,
     include_package_data=True,
-    setup_requires=['pytest-runner'] if testing else [] + [
-        'numpy>=1.8',
+    setup_requires=["pytest-runner"] if testing else [] + [
+        "numpy>=1.8",
     ],
     install_requires=[
-        'nengo>=2.7',
-        'numpy>=1.8',
+        "nengo>=2.7",
+        "numpy>=1.8",
     ],
     extras_require={
-        'prob_cleanup': ['scipy'],
+        "prob_cleanup": ["scipy"],
     },
     tests_require=[
-        'jupyter',
-        'matplotlib>=1.4',
-        'pytest>=2.3',
+        "jupyter",
+        "matplotlib>=2.0",
+        "pytest>=2.3",
     ],
     entry_points={
     },
     classifiers=[  # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: Free for non-commercial use',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
