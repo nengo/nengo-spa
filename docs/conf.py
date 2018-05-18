659c8061becaa728b76c728609cae9c2dd4d4c44
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-05-18 15:44:21 -0400

    Add githubpages Sphinx extension.
    Fixes #154.

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.githubpages',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
