f62a01c41da8ae883c9bfb887080448d09b9bac4
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-07-06 16:20:36 -0400

    Add test case for as_ast_node to keep coverage up.

---
+++ nengo_spa/tests/test_connectors.py
@@ -0,0 +1,16 @@
+import nengo
+import pytest
+
+import nengo_spa as spa
+from nengo_spa.connectors import as_ast_node
+from nengo_spa.exceptions import SpaTypeError
+
+
+@pytest.mark.parametrize('construct_obj', (
+    lambda: nengo.Node([1., 2.]),
+    lambda: [None],
+))
+def test_as_ast_node_error(construct_obj):
+    with spa.Network():
+        with pytest.raises(SpaTypeError):
+            as_ast_node(construct_obj())
