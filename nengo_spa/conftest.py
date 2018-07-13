b12dc73cdc701d1bcd5367d2e0b90283427f8fad
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-07-13 17:05:24 -0400

    Sort test plots into directories by algebra.

--- nengo_spa/conftest.py
+++ nengo_spa/conftest.py
@@ -1,6 +1,9 @@
+import os.path
+
 from nengo.conftest import (  # pylint: disable=unused-import
-    pytest_configure, pytest_runtest_setup, RefSimulator, Simulator, plt, seed,
-    rng)
+    parametrize_function_name, pytest_configure, pytest_runtest_setup,
+    recorder_dirname, RefSimulator, Simulator, seed, rng)
+from nengo.utils.testing import Plotter
 import pytest
 
 from nengo_spa.algebras.cconv import CircularConvolutionAlgebra
@@ -24,3 +27,32 @@ class TestConfig(object):
 def pytest_generate_tests(metafunc):
     if 'algebra' in metafunc.funcargnames:
         metafunc.parametrize('algebra', [a for a in TestConfig.algebras])
+
+
+def recorder_dirname_with_algebra(request, name):
+    dirname = recorder_dirname(request, name)
+    if 'algebra' in request.funcargnames:
+        algebra = request.getfixturevalue('algebra')
+        dirname = os.path.join(dirname, algebra.__name__)
+    return dirname
+
+
+@pytest.fixture
+def plt(request):
+    """A pyplot-compatible plotting interface.
+
+    Please use this if your test creates plots.
+
+    This will keep saved plots organized in a simulator-specific folder,
+    with an automatically generated name. savefig() and close() will
+    automatically be called when the test function completes.
+
+    If you need to override the default filename, set `plt.saveas` to
+    the desired filename.
+    """
+    dirname = recorder_dirname_with_algebra(request, 'plots')
+    plotter = Plotter(
+        dirname, request.module.__name__,
+        parametrize_function_name(request, request.function.__name__))
+    request.addfinalizer(lambda: plotter.__exit__(None, None, None))
+    return plotter.__enter__()
