6438c1946e042a22888f010fd5c2e0f3fe562563
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-07-16 16:04:42 -0400

    Introduce abstract base class for algebras.

---
+++ nengo_spa/algebras/base.py
@@ -0,0 +1,231 @@
+class AbstractAlgebra(object):
+    """Abstract base class for algebras.
+
+    Custom algebras can be defined by implementing the interface of this
+    abstract base class.
+    """
+
+    def is_valid_dimensionality(self, d):
+        """Checks whether *d* is a valid vector dimensionality.
+
+        Parameters
+        ----------
+        d : int
+            Dimensionality
+
+        Returns
+        -------
+        bool
+            *True*, if *d* is a valid vector dimensionality for the use with
+            the algebra.
+        """
+        raise NotImplementedError()
+
+    def make_unitary(self, v):
+        """Returns a unitary vector based on the vector *v*.
+
+        A unitary vector does not change the length of a vector it is bound to.
+
+        Parameters
+        ----------
+        v : (d,) ndarray
+            Vector to base unitary vector on.
+
+        Returns
+        -------
+        ndarray
+            Unitary vector.
+        """
+        raise NotImplementedError()
+
+    def superpose(self, a, b):
+        """Returns the superposition of *a* and *b*.
+
+        This is commonly elementwise addition.
+
+        Parameters
+        ----------
+        a : (d,) ndarray
+            Left operand in superposition.
+        b : (d,) ndarray
+            Right operand in superposition.
+
+        Returns
+        -------
+        (d,) ndarray
+            Superposed vector.
+        """
+        raise NotImplementedError()
+
+    def bind(self, a, b):
+        """Returns the binding of *a* and *b*.
+
+        The resulting vector should in most cases be dissimilar to both inputs.
+
+        Parameters
+        ----------
+        a : (d,) ndarray
+            Left operand in binding.
+        b : (d,) ndarray
+            Right operand in binding.
+
+        Returns
+        -------
+        (d,) ndarray
+            Bound vector.
+        """
+        raise NotImplementedError()
+
+    def invert(self, v):
+        """Invert vector *v*.
+
+        A vector bound to its inverse will result in the identity vector.
+
+        Parameters
+        ----------
+        v : (d,) ndarray
+            Vector to invert.
+
+        Returns
+        -------
+        (d,) ndarray
+            Inverted vector.
+        """
+        raise NotImplementedError()
+
+    def get_binding_matrix(self, v, swap_inputs=False):
+        """Returns the transformation matrix for binding with a fixed vector.
+
+        Parameters
+        ----------
+        v : (d,) ndarray
+            Fixed vector to derive binding matrix for.
+        swap_inputs : bool, optional
+            By default the matrix will be such that *v* becomes the *right*
+            operand in the binding. By setting *swap_inputs*, the matrix will
+            be such that *v* becomes the *left* operand. For binding operations
+            that are commutative (such as circular convolution), this has no
+            effect.
+
+        Returns
+        -------
+        (d, d) ndarray
+            Transformation matrix to perform binding with *v*.
+        """
+        raise NotImplementedError()
+
+    def get_inversion_matrix(self, d):
+        """Returns the transformation matrix for inverting a vector.
+
+        Parameters
+        ----------
+        d : int
+            Vector dimensionality (determines the matrix size).
+
+        Returns
+        -------
+        (d, d) ndarray
+            Transformation matrix to invert a vector.
+        """
+        raise NotImplementedError()
+
+    def implement_superposition(self, n_neurons_per_d, d, n):
+        """Implement neural network for superposing vectors.
+
+        Parameters
+        ----------
+        n_neurons_per_d : int
+            Neurons to use per dimension.
+        d : int
+            Dimensionality of the vectors.
+        n : int
+            Number of vectors to superpose in the network.
+
+        Returns
+        -------
+        tuple
+            Tuple *(net, inputs, output)* where *net* is the implemented
+            `nengo.Network`, *inputs* a sequence of length *n* of inputs to the
+            network, and *output* the network output.
+        """
+        raise NotImplementedError()
+
+    def implement_binding(self, n_neurons_per_d, d, unbind_left, unbind_right):
+        """Implement neural network for binding vectors.
+
+        Parameters
+        ----------
+        n_neurons_per_d : int
+            Neurons to use per dimension.
+        d : int
+            Dimensionality of the vectors.
+        unbind_left : bool
+            Whether the left input should be unbound from the right input.
+        unbind_right : bool
+            Whether the right input should be unbound from the left input.
+
+        Returns
+        -------
+        tuple
+            Tuple *(net, inputs, output)* where *net* is the implemented
+            `nengo.Network`, *inputs* a sequence of the left and the right
+            input in that order, and *output* the network output.
+        """
+        raise NotImplementedError()
+
+    def absorbing_element(self, d):
+        """Return the standard absorbing element of dimensionality *d*.
+
+        An absorbing element will produce a scaled version of itself when bound
+        to another vector. The standard absorbing element is the absorbing
+        element with norm 1.
+
+        Some algebras might not have an absorbing element other than the zero
+        vector. In that case a *NotImplementedError* may be raised.
+
+        Parameters
+        ----------
+        d : int
+            Vector dimensionality.
+
+        Returns
+        -------
+        (d,) ndarray
+            Standard absorbing element.
+        """
+        raise NotImplementedError()
+
+    def identity_element(self, d):
+        """Return the identity element of dimensionality *d*.
+
+        The identity does not change the vector it is bound to.
+
+        Parameters
+        ----------
+        d : int
+            Vector dimensionality.
+
+        Returns
+        -------
+        (d,) ndarray
+            Identity element.
+        """
+        raise NotImplementedError()
+
+    def zero_element(self, d):
+        """Return the zero element of dimensionality *d*.
+
+        The zero element produces itself when bound to a different vector.
+        Usually this will be the zero vector.
+
+        Parameters
+        ----------
+        d : int
+            Vector dimensionality.
+
+        Returns
+        -------
+        (d,) ndarray
+            Zero element.
+        """
+        raise NotImplementedError()
--- nengo_spa/algebras/cconv.py
+++ nengo_spa/algebras/cconv.py
@@ -2,22 +2,21 @@ import nengo
 from nengo.utils.compat import range
 import numpy as np
 
+from nengo_spa.algebras.base import AbstractAlgebra
 from nengo_spa.networks.circularconvolution import CircularConvolution
 
 
-class CircularConvolutionAlgebra(object):
+class _CircularConvolutionAlgebra(AbstractAlgebra):
     """Circular convolution algebra.
 
     Uses element-wise addition for superposition, circular convolution for
     binding with an approximate inverse.
     """
 
-    @classmethod
-    def is_valid_dimensionality(cls, d):
+    def is_valid_dimensionality(self, d):
         return d > 0
 
-    @classmethod
-    def make_unitary(cls, v):
+    def make_unitary(self, v):
         fft_val = np.fft.fft(v)
         fft_imag = fft_val.imag
         fft_real = fft_val.real
@@ -25,54 +24,47 @@ class CircularConvolutionAlgebra(object):
         fft_unit = fft_val / fft_norms
         return np.array((np.fft.ifft(fft_unit, n=len(v))).real)
 
-    @classmethod
-    def superpose(cls, a, b):
+    def superpose(self, a, b):
         return a + b
 
-    @classmethod
-    def bind(cls, a, b):
+    def bind(self, a, b):
         n = len(a)
         if len(b) != n:
             raise ValueError("Inputs must have same length.")
         return np.fft.irfft(np.fft.rfft(a) * np.fft.rfft(b), n=n)
 
-    @classmethod
-    def invert(cls, v):
+    def invert(self, v):
         return v[-np.arange(len(v))]
 
-    @classmethod
-    def get_binding_matrix(cls, v, swap_inputs=False):
+    def get_binding_matrix(self, v, swap_inputs=False):
         D = len(v)
         T = []
         for i in range(D):
             T.append([v[(i - j) % D] for j in range(D)])
         return np.array(T)
 
-    @classmethod
-    def get_inversion_matrix(cls, d):
+    def get_inversion_matrix(self, d):
         return np.eye(d)[-np.arange(d)]
 
-    @classmethod
-    def implement_superposition(cls, n_neurons_per_d, d, n):
+    def implement_superposition(self, n_neurons_per_d, d, n):
         node = nengo.Node(size_in=d)
         return node, n * (node,), node
 
-    @classmethod
-    def implement_binding(cls, n_neurons_per_d, d, unbind_left, unbind_right):
+    def implement_binding(self, n_neurons_per_d, d, unbind_left, unbind_right):
         net = CircularConvolution(
             n_neurons_per_d, d, unbind_left, unbind_right)
         return net, (net.input_a, net.input_b), net.output
 
-    @classmethod
-    def absorbing_element(cls, d):
+    def absorbing_element(self, d):
         return np.ones(d) / np.sqrt(d)
 
-    @classmethod
-    def identity_element(cls, d):
+    def identity_element(self, d):
         data = np.zeros(d)
         data[0] = 1.
         return data
 
-    @classmethod
-    def zero_element(cls, d):
+    def zero_element(self, d):
         return np.zeros(d)
+
+
+CircularConvolutionAlgebra = _CircularConvolutionAlgebra()
