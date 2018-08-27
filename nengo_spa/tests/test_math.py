a65a30eb3430238d6a797268b389d10d38fbca7b
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-08-27 16:40:46 -0400

    Let prob_cleanup raise exception if inaccurate.
    Fixes #203.

--- nengo_spa/tests/test_math.py
+++ nengo_spa/tests/test_math.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from nengo_spa.math import prob_cleanup
@@ -13,3 +14,7 @@ def test_prob_cleanup(rng):
     assert 0.999 > prob_cleanup(0.4, 128, 1000) > 0.997
     assert 0.99 > prob_cleanup(0.4, 128, 10000) > 0.97
     assert 0.9 > prob_cleanup(0.4, 128, 100000) > 0.8
+    assert prob_cleanup(0.4, 128, np.inf) == 0.
+
+    with pytest.raises(ArithmeticError):
+        prob_cleanup(0.2, 2025, np.inf)
