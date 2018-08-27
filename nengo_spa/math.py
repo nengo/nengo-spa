ed4a3aeb92b2e2aed59c4b67c5da62e128ee7970
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-08-27 16:40:34 -0400

    Fix prob_cleanup documentation.

--- nengo_spa/math.py
+++ nengo_spa/math.py
@@ -7,7 +7,7 @@ def prob_cleanup(similarity, dimensions, vocab_size):
     """Estimate the chance of successful cleanup.
 
     This returns the chance that, out of *vocab_size* randomly chosen
-    vectors, at least one of them will be closer to a particular
+    vectors, none of them will be closer to a particular
     vector than the value given by *similarity*. To use this, compare
     your noisy vector with the ideal vector, pass that value in as
     the similarity parameter, and set *vocab_size* to be the number of
