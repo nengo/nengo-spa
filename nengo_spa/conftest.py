4bf6010bc6866984a6c8fef7395cb22d288d2b51
Jan Gosmann <jan@hyper-world.de>
2019-06-10 15:42:12 +0200

    Fix warnings due to outdated Nengo code

--- nengo_spa/conftest.py
+++ nengo_spa/conftest.py
@@ -1,13 +1,41 @@
+import inspect
 import os.path
 
 from nengo.conftest import (  # pylint: disable=unused-import
-    parametrize_function_name, pytest_configure, pytest_runtest_setup,
-    recorder_dirname, RefSimulator, Simulator, seed, rng)
+    pytest_configure, pytest_runtest_setup, recorder_dirname, RefSimulator,
+    Simulator, seed, rng)
 from nengo.utils.testing import Plotter
 import pytest
 
 from nengo_spa.algebras.hrr_algebra import HrrAlgebra
 from nengo_spa.algebras.vtb_algebra import VtbAlgebra
+
+
+# Copied from
+# https://github.com/nengo/nengo/blob/dfeb4335e0f8720e1248466719ca08be9a5badfd/nengo/conftest.py
+# which contains fixes for pytest 4 that are not released at the time of
+# writing this. Also, relying on importing this function from Nengo would also
+# prevent us from testing earlier Nengo releases that do not contain the fixes.
+def parametrize_function_name(request, function_name):
+    """Creates a unique name for a test function.
+
+    The unique name accounts for values passed through
+    ``pytest.mark.parametrize``.
+
+    This function is used when naming plots saved through the ``plt`` fixture.
+    """
+    suffixes = []
+    if 'parametrize' in request.keywords:
+        argnames = []
+        for marker in request.keywords.node.iter_markers("parametrize"):
+            argnames.extend([x.strip() for names in marker.args[::2]
+                             for x in names.split(',')])
+        for name in argnames:
+            value = request.getfixturevalue(name)
+            if inspect.isclass(value):
+                value = value.__name__
+            suffixes.append('{}={}'.format(name, value))
+    return '+'.join([function_name] + suffixes)
 
 
 class TestConfig(object):
