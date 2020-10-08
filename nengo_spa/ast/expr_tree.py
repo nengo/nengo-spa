76d110ac6d4e8a48a70960c71f6b0f971bfbea1a
Jan Gosmann <jan@hyper-world.de>
2020-10-08 20:10:31 +0200

    Generate better SemanticPointer names
    Instead of immediately constructing a string for the SemanticPointer
    name, this uses the expr_tree classes to construct a syntax tree.
    This enables keeping track of operation precedence and allows to
    render a name with only the necessary parenthesis increasing the
    readability of SemanticPointer names.
    
    Furthermore, this implements a visitor for this syntax tree to limit the
    rendered length while preserving correctness of the parenthesis.
    
    Closes #255.

--- nengo_spa/ast/expr_tree.py
+++ nengo_spa/ast/expr_tree.py
@@ -128,3 +128,159 @@ class BinaryOperator(Node):
         if self.precedence >= rhs.precedence:
             rhs = "({})".format(rhs)
         return "{} {} {}".format(lhs, self.value, rhs)
+
+    @property
+    def lhs(self):
+        return self.children[0]
+
+    @property
+    def rhs(self):
+        return self.children[1]
+
+
+class AttributeAccess(Node):
+    __slots__ = ()
+
+    def __new__(cls, value, child):
+        return super().__new__(cls, value, precedence["x.attribute"], (child,))
+
+    def __str__(self):
+        child = self.children[0]
+        operand = str(child)
+        if self.precedence > child.precedence:
+            operand = "({})".format(operand)
+        return operand + "." + self.value
+
+
+class FunctionCall(Node):
+    __slots__ = ()
+
+    def __new__(cls, arguments, child):
+        return super().__new__(
+            cls,
+            [arg if isinstance(arg, Node) else Leaf(arg) for arg in arguments],
+            precedence["x(arguments...)"],
+            (child,),
+        )
+
+    def __str__(self):
+        child = self.children[0]
+        operand = str(child)
+        if self.precedence > child.precedence:
+            operand = "({})".format(operand)
+        return "{}({})".format(operand, ", ".join(str(arg) for arg in self.value))
+
+
+class KeywordArgument(Node):
+    __slots__ = ()
+
+    def __new__(cls, value, child):
+        return super().__new__(cls, value, 0, (child,))
+
+    def __str__(self):
+        return "{}={}".format(self.value, str(self.children[0]))
+
+
+def limit_str_length(expr_tree, max_len):
+    """Returns a modified expression tree with a string length limited to
+    approximately *max_len*."""
+
+    # General idea of the algorithm: Do an in-order traversal and keep track
+    # how much characters we may still use (max_len). When the limit is
+    # exceeded, start replacing nodes with ellipsis and re-adjust max_len to
+    # account for the characters saved through the replacement.
+
+    def visit_Leaf(node):
+        nonlocal max_len
+        if len(node.value) <= 3 or len(node.value) <= max_len:
+            max_len -= len(node.value)
+            return node
+        else:
+            max_len -= 3
+            return Leaf("...")
+
+    def visit_UnaryOperator(node):
+        nonlocal max_len
+        max_len -= len(node.value)
+        if node.precedence >= node.children[0].precedence:
+            max_len -= 2
+        return UnaryOperator(node.value, visit_node(node.children[0]))
+
+    def visit_BinaryOperator(node):
+        nonlocal max_len
+        initial_max_len = max_len
+
+        # Operator + 2 spaces + reserve 3 characters for the second operand, so
+        # that it can be at least turned into an ellipsis
+        max_len -= len(node.value) + 2 + 3
+        lhs = visit_node(node.lhs)
+        max_len += 3  # return reserved characters for the second operand
+        # Adjust for parenthesis due to operator precedence
+        if node.precedence > lhs.precedence:
+            max_len -= 2
+        if node.precedence >= node.rhs.precedence:
+            max_len -= 2
+
+        rhs = visit_node(node.rhs)
+        if max_len < 0:
+            if node.precedence >= node.rhs.precedence:
+                max_len += 2
+            max_len += len(str(rhs)) - 3
+            rhs = Leaf("...")
+        if max_len >= 0:
+            return BinaryOperator(node.value, lhs, rhs)
+        else:
+            max_len = initial_max_len - 3
+            return Leaf("...")
+
+    def visit_AttributeAccess(node):
+        nonlocal max_len
+        initial_max_len = max_len
+        max_len -= len(node.value) + 1
+        child = visit_node(node.children[0])
+        if node.precedence > child.precedence:
+            max_len -= 2
+        if max_len < 0:
+            max_len += len(str(child)) - 5
+            child = Leaf("(...)")
+        if max_len >= 0:
+            return AttributeAccess(node.value, child)
+        else:
+            max_len = initial_max_len - 3
+            return Leaf("...")
+
+    def visit_FunctionCall(node):
+        nonlocal max_len
+        initial_max_len = max_len
+        child = visit_node(node.children[0])
+        max_len -= 2  # parenthesis invoking call
+        if node.precedence > child.precedence:
+            max_len -= 2  # parenthesis around function
+
+        args = []
+        for i, arg in enumerate(node.value):
+            if i > 0:
+                max_len -= 2  # comma + space argument separator
+            arg = visit_node(arg)
+            if i > 0 and max_len < 0:
+                max_len += (
+                    len(str(args[-1])) - 3
+                )  # replacement of previous arg with ellipsis
+                max_len += (
+                    len(str(arg)) + 2
+                )  # adjust for dropped, but processed arg and separator
+                args[-1] = Leaf("...")
+            else:
+                args.append(arg)
+
+        if max_len >= 0:
+            return FunctionCall(args, child)
+        else:
+            max_len = initial_max_len - 3
+            return Leaf("...")
+
+    def visit_node(node):
+        nonlocal max_len, visit_Leaf, visit_UnaryOperator, visit_BinaryOperator, visit_AttributeAccess, visit_FunctionCall
+        return locals()["visit_" + type(node).__name__](node)
+
+    return visit_node(expr_tree)
--- nengo_spa/ast/tests/test_expr_tree.py
+++ nengo_spa/ast/tests/test_expr_tree.py
@@ -1,8 +1,12 @@
 import pytest
 
 from nengo_spa.ast.expr_tree import (
+    AttributeAccess,
     BinaryOperator,
+    FunctionCall,
+    KeywordArgument,
     Leaf,
+    limit_str_length,
     UnaryOperator,
 )
 
@@ -28,6 +32,29 @@ def test_binary_operator_str():
     assert str(BinaryOperator("-", BinaryOperator("+", a, b), c)) == "a + b - c"
 
 
+def test_attribute_access_str():
+    assert str(AttributeAccess("attr", Leaf("foo"))) == "foo.attr"
+    assert (
+        str(AttributeAccess("attr", BinaryOperator("+", Leaf("a"), Leaf("b"))))
+        == "(a + b).attr"
+    )
+
+
+def test_function_call_str():
+    assert str(FunctionCall((), Leaf("fn"))) == "fn()"
+    assert str(FunctionCall(("str1", "str2"), Leaf("fn"))) == "fn(str1, str2)"
+    assert (
+        str(FunctionCall((UnaryOperator("~", Leaf("foo")),), Leaf("fn"))) == "fn(~foo)"
+    )
+    assert (
+        str(FunctionCall((KeywordArgument("kwarg", "val"),), Leaf("fn")))
+        == "fn(kwarg=val)"
+    )
+    assert (
+        str(FunctionCall((), BinaryOperator("+", Leaf("a"), Leaf("b")))) == "(a + b)()"
+    )
+
+
 @pytest.mark.parametrize("op", ["+", "-", "~"])
 def test_unary_operator(op):
     leaf = Leaf("leaf")
@@ -41,3 +68,123 @@ def test_binary_operator(op):
     assert lhs
     assert rhs
     assert str(eval("lhs" + op + "rhs")) == "lhs {} rhs".format(op)
+
+
+@pytest.mark.parametrize(
+    "expr_tree,max_len,expected",
+    [
+        (expr_tree, len(str(expr_tree)), expr_tree)
+        for expr_tree in (
+            Leaf("foo"),
+            Leaf("not_too_long"),
+            -Leaf("foo"),
+            Leaf("a") + Leaf("b"),
+            AttributeAccess("attr", Leaf("foo")),
+            FunctionCall(("arg",), Leaf("foo")),
+        )
+    ]
+    + [
+        (Leaf("foo"), 0, Leaf("foo")),
+        (Leaf("too_long"), len("too_long") - 1, Leaf("...")),
+        (Leaf("too_long"), 3, Leaf("...")),
+        (Leaf("too_long"), 0, Leaf("...")),
+        (
+            Leaf("varA") + Leaf("varB"),
+            len("varA + varB") - 1,
+            Leaf("varA") + Leaf("..."),
+        ),
+        (Leaf("varA") + Leaf("varB"), len("varA + ..."), Leaf("varA") + Leaf("...")),
+        (Leaf("varA") + Leaf("varB"), len("varA + ...") - 1, Leaf("...") + Leaf("...")),
+        (Leaf("varA") + Leaf("varB"), 3, Leaf("...")),
+        (Leaf("a") + Leaf("b"), len("a + b") - 1, Leaf("...")),
+        (
+            (Leaf("a") + Leaf("b")) * Leaf("c"),
+            len("(a + b) * c") - 1,
+            Leaf("...") * Leaf("c"),
+        ),
+        ((Leaf("a") + Leaf("b")) * Leaf("c"), len("(a + b) * c") - 5, Leaf("...")),
+        (
+            (Leaf("a") + Leaf("b")) * Leaf("varC"),
+            len("(a + b) * varC") - 1,
+            (Leaf("a") + Leaf("b")) * Leaf("..."),
+        ),
+        (
+            Leaf("a") * (Leaf("b") + Leaf("c")),
+            len("a * (b + c)") - 1,
+            Leaf("a") * Leaf("..."),
+        ),
+        (
+            Leaf("a") * (Leaf("b") + Leaf("varC")),
+            len("a * (b + varC)") - 1,
+            Leaf("a") * (Leaf("b") + Leaf("...")),
+        ),
+        (
+            Leaf("a") + Leaf("a") * Leaf("b") + Leaf("a") * Leaf("b"),
+            len("a + a * b + a * b") - 2,
+            Leaf("a") + Leaf("a") * Leaf("b") + Leaf("..."),
+        ),
+        (
+            Leaf("a") / (Leaf("a") * Leaf("b")) / (Leaf("a") * Leaf("b")),
+            len("a / (a * b) / (a * b)") - 3,
+            Leaf("a") / (Leaf("a") * Leaf("b")) / Leaf("..."),
+        ),
+        (
+            AttributeAccess("attribute", Leaf("varA") + Leaf("varB")),
+            len("(varA + varB).attribute") - 3,
+            AttributeAccess("attribute", Leaf("(...)")),
+        ),
+        (
+            AttributeAccess("attribute", Leaf("a")),
+            len("a.attribute") - 1,
+            Leaf("..."),
+        ),
+        (FunctionCall([], Leaf("fn_name")), len("...()") - 1, Leaf("...")),
+        (
+            FunctionCall([], Leaf("fn_name")),
+            len("fn_name()") - 1,
+            Leaf("..."),
+        ),
+        (
+            FunctionCall([], Leaf("a") + Leaf("b")),
+            len("(a + b)()") - 1,
+            Leaf("..."),
+        ),
+        (
+            FunctionCall(["arg1", "arg2"], Leaf("fn_name")),
+            len("fn_name(arg1, arg2)") - 1,
+            FunctionCall(["arg1", "..."], Leaf("fn_name")),
+        ),
+        (
+            FunctionCall(["arg1", "arg2"], Leaf("fn_name")),
+            len("fn_name(arg1, arg2)") - 2,
+            FunctionCall(["..."], Leaf("fn_name")),
+        ),
+        (
+            FunctionCall(["arg1", "arg2"], Leaf("fn_name")),
+            len("fn_name(arg1, arg2)") - 8,
+            Leaf("..."),
+        ),
+        (
+            FunctionCall(["arg1", "arg2"], Leaf("a") + Leaf("b")),
+            len("(a + b)(arg1, arg2)") - 1,
+            FunctionCall(["arg1", "..."], Leaf("a") + Leaf("b")),
+        ),
+        (
+            FunctionCall([Leaf("arg1") + Leaf("arg2")], Leaf("fn_name")),
+            len("fn_name(arg1 + arg2)") - 1,
+            FunctionCall([Leaf("arg1") + Leaf("...")], Leaf("fn_name")),
+        ),
+        (
+            Leaf("var") + FunctionCall([], Leaf("fn_name")),
+            len("var + fn_name()") - 1,
+            Leaf("var") + Leaf("..."),
+        ),
+    ],
+)
+def test_limit_str_length(expr_tree, max_len, expected):
+    actual = limit_str_length(expr_tree, max_len)
+    assert (
+        actual == expected
+    ), 'When limiting the expression "{expr}" to length {max_len}, "{expected}" is expected, but got "{actual}"'.format(
+        expr=str(expr_tree), max_len=max_len, expected=str(expected), actual=str(actual)
+    )
