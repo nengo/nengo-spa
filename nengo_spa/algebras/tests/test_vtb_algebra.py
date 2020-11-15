62a1e5e5e7e8b217df53166ef45e11c57ce6e11d
Jan Gosmann <jan@hyper-world.de>
2020-11-15 15:22:16 +0100

    Implement sign for VtbAlgebra

--- nengo_spa/algebras/tests/test_vtb_algebra.py
+++ nengo_spa/algebras/tests/test_vtb_algebra.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pytest
 
-from nengo_spa.algebras.vtb_algebra import VtbAlgebra
+from nengo_spa.algebras.vtb_algebra import VtbAlgebra, VtbSign
 from nengo_spa.semantic_pointer import SemanticPointer
 from nengo_spa.vector_generation import UnitLengthVectors
 
@@ -26,3 +27,20 @@ def test_get_swapping_matrix(rng):
 
     m = VtbAlgebra().get_swapping_matrix(64)
     assert np.allclose(VtbAlgebra().bind(a, b), np.dot(m, VtbAlgebra().bind(b, a)))
+
+
+@pytest.mark.parametrize(
+    "sign",
+    [VtbSign(-1), VtbSign(1)],
+)
+def test_sign_and_abs(sign):
+    pytest.importorskip("scipy")
+    d = 16
+    algebra = VtbAlgebra()
+    base = next(UnitLengthVectors(d))
+    # TODO replace the following line with the canonical methods of generating
+    # positive definite vectors for the algebra
+    abs_v = np.dot(algebra.get_binding_matrix(base), base)
+    v = algebra.bind(sign.to_vector(d), abs_v)
+    assert algebra.sign(v) == sign
+    assert np.allclose(algebra.abs(v), abs_v)
