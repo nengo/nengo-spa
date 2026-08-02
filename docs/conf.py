518bbae4de19a4fd6c385ad352cea3fe19ecc755
Trevor Bekolay <tbekolay@gmail.com>
2026-08-02 08:52:57 -0500

    Remove analytics tracking from docs config
    Regenerated docs/conf.py with nengo-bones, which no longer
    injects Google Analytics and Matomo tracking into built docs.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,6 @@ extensions = [
     "sphinx.ext.todo",
     "nbsphinx",
     "nengo_sphinx_theme",
-    "nengo_sphinx_theme.ext.backoff",
     "nengo_sphinx_theme.ext.redirects",
     "nengo_sphinx_theme.ext.sourcelinks",
     "notfound.extension",
@@ -76,7 +75,7 @@ user_agent = "nengo_spa"
 
 project = "NengoSPA"
 authors = "Applied Brain Research"
-copyright = "2013-2023 Applied Brain Research"
+copyright = "2013-2026 Applied Brain Research"
 version = ".".join(nengo_spa.__version__.split(".")[:2])  # Short X.Y version
 release = nengo_spa.__version__  # Full version, with tags
 
@@ -92,36 +91,6 @@ html_favicon = str(pathlib.Path("_static", "favicon.ico"))
 html_theme_options = {
     "nengo_logo": "nengo-spa-full-light.svg",
     "nengo_logo_color": "#d40000",
-    "analytics": """
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GT8XEDLTMJ"></script>
-        <script>
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-         gtag('config', 'G-GT8XEDLTMJ');
-        </script>
-        <!-- End Google tag (gtag.js) -->
-        <!-- Matomo -->
-        <script>
-         var _paq = window._paq = window._paq || [];
-         _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-         _paq.push(["setCookieDomain", "*.appliedbrainresearch.com"]);
-         _paq.push(["setDomains", ["*.appliedbrainresearch.com","*.edge.nengo.ai","*.forum.nengo.ai","*.nengo.ai"]]);
-         _paq.push(["enableCrossDomainLinking"]);
-         _paq.push(["setDoNotTrack", true]);
-         _paq.push(['trackPageView']);
-         _paq.push(['enableLinkTracking']);
-         (function() {
-           var u="https://appliedbrainresearch.matomo.cloud/";
-           _paq.push(['setTrackerUrl', u+'matomo.php']);
-           _paq.push(['setSiteId', '3']);
-           var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-           g.async=true; g.src='//cdn.matomo.cloud/appliedbrainresearch.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-         })();
-        </script>
-        <!-- End Matomo Code -->
-    """,
 }
 html_redirects = [
     ("dev_syntax.html", "dev-syntax.html"),
